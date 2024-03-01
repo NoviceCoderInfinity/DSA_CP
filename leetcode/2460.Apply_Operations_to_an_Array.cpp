47959f74699a626f358f998f5640fc69785deaaf
NoviceCoderInfinity <anupamscience06@gmail.com>
2024-03-01 23:58:00 +0530

    Added Solution for LeetCode Problem 2460

--- leetcode/2460.Apply_Operations_to_an_Array.cpp
+++ leetcode/2460.Apply_Operations_to_an_Array.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
@@ -14,6 +15,30 @@ public:
 
         while(insert < nums.size()) {
             nums[insert++] = 0;
+        }
+        return nums;
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int insert = 0, i = 0;
+        for ( ; i < nums.size() - 1; ++i) {
+            if (nums[i] == nums[i+1]) {
+                nums[i] *= 2; nums[i+1] = 0;
+            }
+
+            if (nums[i]) {
+                if (i != insert) {swap(nums[i], nums[insert]);}
+                ++insert;
+            }
+        }
+
+        if (nums[i]) {
+            if (i != insert) {swap(nums[i], nums[insert]);}
+            ++insert;
         }
         return nums;
     }
