27e8653a0440a58d0e85439db5ec4c40df73e624
NoviceCoderInfinity <anupamscience06@gmail.com>
2024-08-26 19:52:35 +0530

    Added another solution for LeetCode problem 590

--- leetcode/590.N-ary_Tree_Postorder_Traversal.cpp
+++ leetcode/590.N-ary_Tree_Postorder_Traversal.cpp
@@ -18,6 +18,8 @@ public:
 };
 */
 
+
+//SOLUTION 1: RECURSIVE SOLUTION
 class Solution {
     vector<int> post_order_traversal;
 public:
@@ -31,5 +33,23 @@ public:
     vector<int> postorder(Node* root) {
         add_elements(root);
         return post_order_traversal;
+    }
+};
+
+//SOLUTION 2: ITERATIVE SOLUTION
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if (root == nullptr) {return {};}
+        vector<int> result;
+        stack<Node*> st; st.push(root);
+        while(st.size()) {
+            Node* temp = st.top(); st.pop();
+            result.push_back(temp->val);
+            for (auto child: temp->children) {st.push(child);}
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
