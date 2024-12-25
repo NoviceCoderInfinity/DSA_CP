40eff20c6e1d7e83bcd6cd9b71aa64164fb94859
NoviceCoderInfinity <anupamscience06@gmail.com>
2024-12-25 08:42:02 +0000

    Added Solution for LeetCode Problem 8

--- leetcode/8.String_to_Integer_atoi.cpp
+++ leetcode/8.String_to_Integer_atoi.cpp
@@ -23,3 +23,80 @@ public:
         return positive ? result : -result;
     }
 };
+
+
+// Discrete Finite Automata Solution
+enum State {q0, q1, q2, qd};
+
+class StateMachine {
+    State curr;
+    int result;
+    bool pos;
+
+    bool sanityCheck(int dig) {
+        return ((result > (INT_MAX - dig) / 10) || (result < (INT_MIN + dig) / 10));
+    }
+
+    void to_q1(char& ch) {
+        curr = q1;
+        add_to_result(ch - '0');
+    }
+
+    void to_q2(char& ch){
+        curr = q2;
+        if (ch == '-') {pos = false;}
+        else {pos = true;}
+    }
+
+    void to_qd() {
+        curr = qd;
+    }
+
+    void add_to_result(int dig) {
+        if (sanityCheck(dig)) {
+            if (pos) {result = INT_MAX;}
+            else {result = INT_MIN; pos = true;}
+            to_qd();
+        }
+        else {result = result * 10 + dig;}
+    }
+
+public:
+    StateMachine() {
+        curr = q0; result = 0; pos = true;
+    }
+
+    void transition(char& ch) {
+        if (curr == q0) {
+            if (ch == ' ') {return;}
+            else if (ch == '-' || ch == '+') {return to_q2(ch);}
+            else if (ch >= '0' && ch <= '9') {return to_q1(ch);}
+            else {return to_qd();}
+        }
+        else if (curr == q1 || curr == q2) {
+            if (ch >= '0' && ch <= '9') {to_q1(ch);}
+            else {to_qd();}
+        }
+    }
+
+    int getResult() {
+        return pos ? result : -result;
+    }
+
+    State getState() {
+        return curr;
+    }
+};
+
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        StateMachine DFA;
+        for (int i = 0; i < s.size(); ++i) {
+            DFA.transition(s[i]);
+        }
+
+        return DFA.getResult();
+    }
+};
