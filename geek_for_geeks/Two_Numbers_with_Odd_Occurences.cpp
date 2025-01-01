62f0af6c3fbfdcab93ed28f7a23c8802b034d6b9
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-01-01 20:15:41 +0000

    Added Solution for Geek for Geeks / Two Numbers with Odd Occurences

---
+++ geek_for_geeks/Two_Numbers_with_Odd_Occurences.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int Arr[], long long int N)  
+    {
+        // code here
+        set<long long int> elements;
+        for (long long int i = 0; i < N; ++i) {
+            long long int num = Arr[i];
+            if (elements.find(num) == elements.end()) {elements.insert(num);}
+            else {elements.erase(num);}
+        }
+        vector<long long int> v(elements.begin(), elements.end());
+        sort(v.rbegin(), v.rend());
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int N;
+        cin>>N;
+        long long int Arr[N];
+        for(long int i = 0;i < N;i++)
+        cin>>Arr[i];
+        Solution ob;
+        vector<long long int>ans=ob.twoOddNum(Arr,N);
+        for(long long int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
