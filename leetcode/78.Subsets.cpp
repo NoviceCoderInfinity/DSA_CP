a101bed660db00d773b2df71a002f59090b07f6f
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-01-01 13:25:58 +0000

    Added Solution for LeetCode Problem 78

--- leetcode/78.Subsets.cpp
+++ leetcode/78.Subsets.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -11,6 +12,29 @@ public:
                 answer.push_back(tmp);
             }
         }
+        return answer;
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    void next_iter(string& iter) {
+        int carry = 1;
+        for(int i = iter.size() - 1; i >= 0; --i) {
+            if (carry == 0) {break;}
+            if (iter[i] == '0' && carry) {iter[i] = '1'; carry = 0;}
+            else if (iter[i] == '1' && carry) {iter[i] = '0'; carry = 1;}}
+            return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> answer;
+        string iter(nums.size(), '0');
+        for(int i = 0; i < (int)(pow(2, nums.size())); ++i) {
+            vector<int> temp;
+            for(int j = 0; j < iter.size(); ++j) {
+                if (iter[j] == '1') {temp.push_back(nums[j]);}}
+            answer.push_back(temp); next_iter(iter);}
         return answer;
     }
 };
