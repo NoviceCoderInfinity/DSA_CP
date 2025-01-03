703e66b5e8ac9d761a23d399a1751e60d2551bf9
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-01-03 04:19:18 +0000

    Added Solution for LeetCode Problem 2270

--- leetcode/2270.Number_of_Ways_to_Split_Array.cpp
+++ leetcode/2270.Number_of_Ways_to_Split_Array.cpp
@@ -1,3 +1,25 @@
+// Solution 1
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int ways_split = 0, n = nums.size();
+
+        long long int left_sum = 0, right_sum = 0;
+        for (int num: nums) {
+            right_sum += static_cast<long long int>(num);
+        }
+
+        for (int i = 0; i < n - 1; ++i) {
+            left_sum += static_cast<long long int>(nums[i]);
+            right_sum -= static_cast<long long int>(nums[i]);
+            ways_split += (left_sum >= right_sum);
+        }
+
+        return ways_split;
+    }
+};
+
+// Solution 2
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
