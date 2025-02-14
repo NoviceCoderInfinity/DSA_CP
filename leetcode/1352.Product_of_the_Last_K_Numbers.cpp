a04e04bd2d29bba548521f67afd1641cbd777526
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-02-14 11:56:49 +0530

    Added Solution for LeetCode Problem 1352

--- leetcode/1352.Product_of_the_Last_K_Numbers.cpp
+++ leetcode/1352.Product_of_the_Last_K_Numbers.cpp
@@ -1,3 +1,5 @@
+// Solution 1: O(k) solution
+
 class ProductOfNumbers {
     vector<int> nums;
 public:
@@ -16,6 +18,33 @@ public:
             if (!product) {break;}
         }
         return product;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
+
+
+// Solution 2: O(1) Solution
+class ProductOfNumbers {
+    vector<int> prefix_mul;
+public:
+    ProductOfNumbers() {
+        prefix_mul = {1};
+    }
+    
+    void add(int num) {
+        if (num == 0) {prefix_mul = {1};}
+        else {prefix_mul.push_back(num * prefix_mul.back());}
+    }
+    
+    int getProduct(int k) {
+        if (k >= prefix_mul.size()) {return 0;}
+        return prefix_mul.back() / prefix_mul[prefix_mul.size() - k - 1];
     }
 };
 
