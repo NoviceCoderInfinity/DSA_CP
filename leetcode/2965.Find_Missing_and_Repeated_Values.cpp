e29f8d0e47a3211d219d9e2347307acb3163c98a
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-03-06 10:10:51 +0000

    Added Solution for LeetCode Problem 2965

--- leetcode/2965.Find_Missing_and_Repeated_Values.cpp
+++ leetcode/2965.Find_Missing_and_Repeated_Values.cpp
@@ -1,4 +1,4 @@
-// Solution 1
+// Solution 1: Time Complexity O(n^2) - two O(n^2) loops; Space Complexity O(n^2)
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
@@ -13,6 +13,28 @@ public:
         for (int i = 0; i < n*n; ++i) {
             if (elements_frequency[i] == -1) {result[0] = i + 1;}
             else if (elements_frequency[i] == 1) {result[1] = i + 1;}
+        }
+        return result;
+    }
+};
+
+// Solution 2: Time Complexity O(n^2) - one O(n^2) loop; Space Complexity O(n^2)
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size(); int num = 1;
+        set<int> elements;
+        vector<int> result = {0, 0};
+        for (vector<int> v: grid) {
+            for (int i = 0; i < n; ++i) {
+                result[1] ^= num++; 
+                if (elements.find(v[i]) != elements.end()) {
+                    result[0] = v[i];
+                    continue;
+                }
+                elements.insert(v[i]);
+                result[1] ^= v[i];
+            }
         }
         return result;
     }
