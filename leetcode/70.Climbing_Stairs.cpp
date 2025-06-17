13c13c1847b7c9f7e3d4616628e5d3b2bb53db7c
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-06-17 08:22:59 +0000

    Added new approach for LeetCode Problem file

--- leetcode/70.Climbing_Stairs.cpp
+++ leetcode/70.Climbing_Stairs.cpp
@@ -43,3 +43,20 @@ public:
         return dp[n];
     }
 };
+
+// Fibonacci Sequence
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 3) {return n;}
+        int a = 2, b = 3;
+        for (int i = 4; i <= n; ++i) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
