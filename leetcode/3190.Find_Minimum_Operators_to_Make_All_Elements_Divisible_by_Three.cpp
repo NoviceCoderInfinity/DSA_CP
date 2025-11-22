47cdf3eb975aef21ae4dae87f9edb3d1ee9faa14
NoviceCoderInfinity <anupamscience06@gmail.com>
2025-11-22 23:56:36 +0530

    chore: Solution for LeetCode 3190, Daily Question 2025-11-22

---
+++ leetcode/3190.Find_Minimum_Operators_to_Make_All_Elements_Divisible_by_Three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int operations = 0;
+        for (int num: nums) {
+            if (num % 3 == 0) continue;
+            ++operations;
+        }
+        return operations;
+    }
+};
