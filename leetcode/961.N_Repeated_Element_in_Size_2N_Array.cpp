b186ad8802298b499845d7c919b7a271e0644592
Anupam Rawat <anupamscience06@gmail.com>
2026-01-02 04:44:22 +0000

    chore: Solution for LeetCode Problem 961; Daily Question 2026-01-02

--- leetcode/961.N_Repeated_Element_in_Size_2N_Array.cpp
+++ leetcode/961.N_Repeated_Element_in_Size_2N_Array.cpp
@@ -1,3 +1,4 @@
+// Hash Set
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
@@ -9,9 +10,21 @@ public:
         }
 
         for (auto const& [key, val]: mp) {
-            if (val > 1) return key;
+            if (val > 1) return key; // for N duplicates in 2N array, there must be N unique, so any duplicate is the answer
         }
         
         return -1;
+    }
+};
+
+// Sorting based approach
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int mid = nums.size() / 2;
+        if (nums[mid] != nums[mid+1]) {return nums[mid-1];}
+        return nums[mid];
     }
 };
