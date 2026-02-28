fd014f9bf3ac074d1e3acb7662c469216a7db76a
Anupam Rawat <anupam.rawat.iitb@gmail.com>
2026-02-28 14:12:32 +0530

    chore: leetcode solution for problem 1680; daily question 2026-02-28

---
+++ leetcode/1680.Concatenation_of_Consecutive_Binary_Numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long result = 0;
+        int digits = 0;
+
+        for (int i = 1; i <= n; i++) {            
+            if ((i & (i - 1)) == 0) {
+                digits++;
+            }
+
+            result = ((result << digits) | i) % MOD;
+        }
+
+        return result;
+    }
+};
