ba8f5617bb86fd7c3048f2b95b5c3629c4681c5a
Anupam Rawat <anupam.rawat.iitb@gmail.com>
2026-04-19 15:16:09 +0530

    chore: leetcode problem 1855; daily question of the day 2026-04-19; approach: Binary Search

--- leetcode/1855.Maximum_Distance_Between_a_Pair_of_Values.cpp
+++ leetcode/1855.Maximum_Distance_Between_a_Pair_of_Values.cpp
@@ -1,3 +1,6 @@
+// Approach 1: Two pointers
+// Time Complexity: O(m + n)
+// Space Complexity: O(1)
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
@@ -11,6 +14,42 @@ public:
                 max_dist = max(max_dist, idx2 - idx1);
                 ++idx2;
             }
+        }
+
+        return max_dist;
+    }
+};
+
+// Approach 2: Binary Search
+// Time Complexity: O(m log n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int binary_search(vector<int>& nums1, int right, int val2) {
+        int left = 0, ans = right + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums1[mid] <= val2) {
+                ans = mid;
+                right = mid - 1; 
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (ans >= nums1.size()) return 0;
+        return ans;
+    }
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        int idx1 = n1 -1, idx2 = n2 - 1;
+        int max_dist = 0;
+
+        while (idx1 >= 0 && idx2 >= 0) {
+            idx1 = min(idx1, idx2);
+            idx1 = binary_search(nums1, idx1, nums2[idx2]);
+            if (nums1[idx1] <= nums2[idx2] && idx1 <= idx2)
+                max_dist = max(max_dist, idx2 - idx1);
+            --idx2;
         }
 
         return max_dist;
